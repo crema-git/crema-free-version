e81865dbd3a075c7f78ffa37de74207e57e9d776
Gourav Dey <gouravdey@mastersindia.co>
2022-12-18 19:52:19 +0530

    removing unwanted code

--- libs/redux-toolkit/libs/src/lib/store/index.js
+++ libs/redux-toolkit/libs/src/lib/store/index.js
@@ -1,14 +1,14 @@
-import { applyMiddleware, compose, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import reducers from '../reducers';
-import { routerMiddleware } from 'connected-react-router';
-import thunk from 'redux-thunk';
+// import { routerMiddleware } from 'connected-react-router';
+// import thunk from 'redux-thunk';
 
-const createBrowserHistory = require('history').createBrowserHistory;
-const history = createBrowserHistory();
-const routeMiddleware = routerMiddleware(history);
+// const createBrowserHistory = require('history').createBrowserHistory;
+// const history = createBrowserHistory();
+// const routeMiddleware = routerMiddleware(history);
 
-const middlewares = [thunk, routeMiddleware];
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const middlewares = [thunk, routeMiddleware];
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function ConfigureStore(initialState) {
   const store = configureStore({
