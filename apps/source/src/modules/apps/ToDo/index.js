d9059352511f7d1cd36135f4e9afd337588940e1
Gourav Dey <gouravdey@mastersindia.co>
2022-12-21 22:44:48 +0530

    updated todo modules

--- apps/source/src/modules/apps/ToDo/index.js
+++ apps/source/src/modules/apps/ToDo/index.js
@@ -1,3 +1,66 @@
-import ToDo from '@crema/modules/apps/ToDo';
+import React from 'react';
+import TaskSideBar from './TaskSideBar/index';
+import TasksList from './TasksList';
+import TaskDetail from './TaskDetail';
+import PropTypes from 'prop-types';
+import { useIntl } from 'react-intl';
+import AppsContainer from '@crema/components/AppsContainer';
+import clsx from 'clsx';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { useGetDataApi } from '@crema/utility/APIHooks';
+
+const ToDo = () => {
+  const { id } = useParams();
+
+  const [
+    { apiData: taskLists, loading },
+    { setQueryParams, setData, reCallAPI },
+  ] = useGetDataApi('/api/todo/task/list', {}, {}, false);
+
+  const { messages } = useIntl();
+  return (
+    <AppsContainer
+      title={messages['todo.todoApp']}
+      sidebarContent={<TaskSideBar reCallAPI={reCallAPI} />}
+    >
+      <TasksList
+        taskLists={taskLists}
+        loading={loading}
+        setQueryParams={setQueryParams}
+        setData={setData}
+      />
+      <Box
+        sx={{
+          transition: 'all 0.5s ease',
+          transform: 'translateX(100%)',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          zIndex: 1,
+          opacity: 0,
+          visibility: 'hidden',
+          backgroundColor: 'background.paper',
+          '&.show': {
+            transform: 'translateX(0)',
+            opacity: 1,
+            visibility: 'visible',
+          },
+        }}
+        className={clsx({
+          show: id,
+        })}
+      >
+        <TaskDetail />
+      </Box>
+    </AppsContainer>
+  );
+};
 
 export default ToDo;
+
+ToDo.propTypes = {
+  match: PropTypes.object,
+};
--- libs/modules/src/lib/apps/ToDo/index.js
+++ libs/modules/src/lib/apps/ToDo/index.js
@@ -1,66 +1,27 @@
-import React from 'react';
-import TaskSideBar from './TaskSideBar/index';
-import TasksList from './TasksList';
-import TaskDetail from './TaskDetail';
-import PropTypes from 'prop-types';
-import { useIntl } from 'react-intl';
-import AppsContainer from '@crema/components/AppsContainer';
-import clsx from 'clsx';
-import { useParams } from 'react-router-dom';
-import { Box } from '@mui/material';
-import { useGetDataApi } from '@crema/utility/APIHooks';
+import CommentsLists from './TaskDetail/TaskDetailBody/CommentsList';
+import AssignedStaff from './TaskDetail/TaskDetailBody/AssignedStaff';
+import TodoDatePicker from './TaskDetail/TaskDetailBody/DatePicker';
+import EditButton from './TaskDetail/TaskDetailBody/EditButton';
+import TaskCreatedByInfo from './TaskDetail/TaskDetailBody/TaskCreatedByInfo';
+import TaskLabelItem from './TaskSideBar/LabelItem';
+import TaskCalender from './TasksList/TaskCalendar';
+import SelectTasksDropdown from './TasksList/TaskContentHeader/SelectTasksDropdown';
+import ViewSelectButtons from './TasksList/TaskContentHeader/ViewSelectButtons';
+import TaskLabels from './TasksList/TaskListItem/Labels';
+import TaskListItem from './TasksList/TaskListItem';
+import TaskListItemMobile from './TasksList/TaskListItemMobile';
 
-const ToDo = () => {
-  const { id } = useParams();
-
-  const [
-    { apiData: taskLists, loading },
-    { setQueryParams, setData, reCallAPI },
-  ] = useGetDataApi('/api/todo/task/list', {}, {}, false);
-
-  const { messages } = useIntl();
-  return (
-    <AppsContainer
-      title={messages['todo.todoApp']}
-      sidebarContent={<TaskSideBar reCallAPI={reCallAPI} />}
-    >
-      <TasksList
-        taskLists={taskLists}
-        loading={loading}
-        setQueryParams={setQueryParams}
-        setData={setData}
-      />
-      <Box
-        sx={{
-          transition: 'all 0.5s ease',
-          transform: 'translateX(100%)',
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          zIndex: 1,
-          opacity: 0,
-          visibility: 'hidden',
-          backgroundColor: 'background.paper',
-          '&.show': {
-            transform: 'translateX(0)',
-            opacity: 1,
-            visibility: 'visible',
-          },
-        }}
-        className={clsx({
-          show: id,
-        })}
-      >
-        <TaskDetail />
-      </Box>
-    </AppsContainer>
-  );
-};
-
-export default ToDo;
-
-ToDo.propTypes = {
-  match: PropTypes.object,
+export {
+  TaskLabels,
+  CommentsLists,
+  AssignedStaff,
+  TodoDatePicker,
+  EditButton,
+  TaskCreatedByInfo,
+  TaskLabelItem,
+  TaskCalender,
+  SelectTasksDropdown,
+  ViewSelectButtons,
+  TaskListItem,
+  TaskListItemMobile,
 };
