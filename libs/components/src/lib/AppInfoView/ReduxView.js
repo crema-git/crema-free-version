49683e026473c33a8e595d6bfa7b0c0eacde3783
Gourav Dey <gouravdey@mastersindia.co>
2023-01-08 15:05:45 +0530

    Fixed AppInfoView issue

---
+++ libs/components/src/lib/AppInfoView/ReduxView.js
@@ -0,0 +1,43 @@
+import AppMessageView from '../AppMessageView';
+import AppLoader from '../AppLoader';
+import { HIDE_MESSAGE } from '@crema/constants';
+import { useDispatch, useSelector } from 'react-redux';
+
+const AppInfoViewRedux = () => {
+  const { error, loading, message } = useSelector(({ common }) => common);
+  const dispatch = useDispatch();
+  const clearInfoView = () => {
+    dispatch(dispatch({ type: HIDE_MESSAGE }));
+  };
+
+  const showMessage = () => {
+    return (
+      <AppMessageView
+        variant='success'
+        message={message.toString()}
+        clearInfoView={clearInfoView}
+      />
+    );
+  };
+
+  const showError = () => {
+    return (
+      <AppMessageView
+        variant='error'
+        message={error.toString()}
+        clearInfoView={clearInfoView}
+      />
+    );
+  };
+
+  return (
+    <>
+      {loading && <AppLoader />}
+
+      {message && showMessage()}
+      {error && showError()}
+    </>
+  );
+};
+
+export default AppInfoViewRedux;
--- libs/components/src/lib/AppInfoView/index.js
+++ libs/components/src/lib/AppInfoView/index.js
@@ -1,53 +1,12 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import AppMessageView from '../AppMessageView';
-import AppLoader from '../AppLoader';
-import { HIDE_MESSAGE } from '@crema/constants';
-// import {
-//   useInfoViewActionsContext,
-//   useInfoViewContext,
-// } from '@crema/context/InfoViewContextProvider';
+import AppInfoViewContext from './ContextView';
+import AppInfoViewRedux from './ReduxView';
 
 const AppInfoView = () => {
-  // // Only for Context APIs
-  // const { error, loading, message } = useInfoViewContext();
-  // const { clearInfoView } = useInfoViewActionsContext();
-
-  // Only for redux
-  const { error, loading, message } = useSelector(({ common }) => common);
-  const dispatch = useDispatch();
-  const clearInfoView = () => {
-    dispatch(dispatch({ type: HIDE_MESSAGE }));
-  };
-
-  const showMessage = () => {
-    return (
-      <AppMessageView
-        variant='success'
-        message={message.toString()}
-        clearInfoView={clearInfoView}
-      />
-    );
-  };
-
-  const showError = () => {
-    return (
-      <AppMessageView
-        variant='error'
-        message={error.toString()}
-        clearInfoView={clearInfoView}
-      />
-    );
-  };
-
-  return (
-    <>
-      {loading && <AppLoader />}
-
-      {message && showMessage()}
-      {error && showError()}
-    </>
-  );
+  if (process.env.NX_STATE_TYPE === 'context') {
+    return <AppInfoViewContext />;
+  }
+  return <AppInfoViewRedux />;
 };
 
 export default AppInfoView;
