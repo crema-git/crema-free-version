0203d41fa135cdaceea3955b37c19c820615d298
Yogendra <yogendrasingh@mastersindia.co>
2023-01-11 00:03:59 +0530

    fixed some minor issues

--- apps/source-toolkit/src/toolkit/store/index.js
+++ apps/source-toolkit/src/toolkit/store/index.js
@@ -1,29 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit';
 import reducers from '../reducers';
-// import { routerMiddleware } from 'connected-react-router';
-// import thunk from 'redux-thunk';
-
-// const createBrowserHistory = require('history').createBrowserHistory;
-// const history = createBrowserHistory();
-// const routeMiddleware = routerMiddleware(history);
-
-// const middlewares = [thunk, routeMiddleware];
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-export default function ConfigureStore(initialState) {
-  const store = configureStore({
+export default function ConfigureStore() {
+  return configureStore({
     reducer: reducers(),
-    // initialState,
-    // composeEnhancers(applyMiddleware(...middlewares))
   });
-
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers/index', () => {
-      const nextRootReducer = require('../reducers');
-      store.replaceReducer(nextRootReducer);
-    });
-  }
-
-  return store;
 }
